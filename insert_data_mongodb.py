da12b5816e115a901582cd6be691ec836336ef6b
unknown <junho4379@gmail.com>
2024-06-21 18:40:05 +0900

    feat : insert mongodb

--- insert_data_mongodb.py
+++ insert_data_mongodb.py
@@ -2,31 +2,25 @@ import pandas as pd
 from pymongo import MongoClient
 
 # CSV 파일 읽기
-df = pd.read_csv('Apartment.csv', encoding='utf-8')
-df1 = pd.read_csv('ApartmentAdd.csv', encoding='utf-8')
-df2 = pd.read_csv('Officetel.csv', encoding='utf-8')
-df3 = pd.read_csv('OfficetelAdd.csv', encoding='utf-8')
+df = pd.read_csv('./dataNop/apartment.csv', encoding='utf-8')
+df2 = pd.read_csv('./dataNop/officetel.csv', encoding='utf-8')
 
 # MongoDB 클라이언트 설정
-client = MongoClient('mongodb://175.106.97.107:27017/')
+username = 'root'  # 실제 MongoDB 사용자 이름
+password = 'root'  # 실제 MongoDB 사용자 비밀번호
+client = MongoClient(f'mongodb://{username}:{password}@175.106.97.107:27017/')
 db = client['mongo_db']  # 데이터베이스 이름 설정
-collection_lands = db['lands']  # 컬렉션 이름 설정
-collection_lands_add = db['lands_add']  # 컬렉션 이름 설정
+apartments_lands = db['apartments']  # 컬렉션 이름 설정
+officetels_lands = db['officetels']  # 컬렉션 이름 설정
 
 # Apartment 데이터 삽입
 lands_data = df.to_dict(orient='records')
-collection_lands.insert_many(lands_data)
+apartments_lands.insert_many(lands_data)
 
-# ApartmentAdd 데이터 삽입
-lands_add_data = df1.to_dict(orient='records')
-collection_lands_add.insert_many(lands_add_data)
 
 # Officetel 데이터 삽입
 lands_data = df.to_dict(orient='records')
-collection_lands.insert_many(lands_data)
+officetels_lands.insert_many(lands_data)
 
-# OfficetelAdd 데이터 삽입
-lands_add_data = df1.to_dict(orient='records')
-collection_lands_add.insert_many(lands_add_data)
 
 print("Data inserted successfully into MongoDB")
